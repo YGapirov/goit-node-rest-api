8fc33b14a0813992b8ef7da2284709d5c2851b0f
Yurii <gapirov21@gmail.com>
2024-02-05 23:38:47 +0200

    need fix logout and current

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,6 +2,12 @@ const HttpError = require("../helpers/HttpError.js");
 const controllerWrapper = require("../helpers/controllerWrapper.js");
 const User = require("../models/users.js");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const { JWT_SECRET } = process.env;
 
 const register = async (req, res, next) => {
   const { email, password, subscription } = req.body;
@@ -41,10 +47,21 @@ const login = async (req, res, next) => {
     throw HttpError(401, "Email or password is wrong");
   }
 
-  res.json({ token: "<Token>" });
+  const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: "10h" });
+  res.json({ token });
 };
+
+const logout = async (req, res) => {
+  const { _id } = req.user;
+  await User.findByIdAndUpdate(_id, { token: "" });
+  res.status(204).json({ message: "No Content" });
+};
+
+const getCurrent = async (req, res, next) => {};
 
 module.exports = {
   register: controllerWrapper(register),
   login: controllerWrapper(login),
+  logout: controllerWrapper(logout),
+  getCurrent: controllerWrapper(getCurrent),
 };
--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 const dotenv = require("dotenv");
 const app = require("./app");
 
 dotenv.config();
 const { DB_HOST } = process.env;
+
+// const JWT_SECRET = "WARNING secret information";
+// async function main() {
+//   const payload = { id: "asdasdasdasdasdasd", email: "dg@mail.com" };
+//   const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "15s" });
+//   console.log({ token });
+
+//   try {
+//     const isValid = jwt.verify(token, JWT_SECRET);
+//     console.log({ isValid });
+//   } catch (error) {
+//     console.error("Not authorized", error);
+//   }
+
+//   //expired error
+
+//   try {
+//     const expiredToken =
+//       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFzZGFzZGFzZGFzZGFzZGFzZCIsImVtYWlsIjoiZGdAbWFpbC5jb20iLCJpYXQiOjE3MDcxNjI3NTcsImV4cCI6MTcwNzE2Mjc3Mn0.I7duVM8Zk5Y7YFUrJV3F8xZVgS00pbnbJrzBxRKkoPI";
+//     const isExpired = jwt.verify(expiredToken, JWT_SECRET);
+//     console.log({ isExpired });
+//   } catch (error) {
+//     console.error("Not authorized", error);
+//   }
+// }
+// main();
 
 mongoose
   .connect(DB_HOST)
