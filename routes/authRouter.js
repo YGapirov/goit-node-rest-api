7caf7057f95d6c8a5b45a826d988ce28f66e55e7
Yurii <gapirov21@gmail.com>
2024-02-06 23:01:58 +0200

    fix prettier

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,16 +1,8 @@
 const express = require("express");
-const validateBody = require("../middlewares/validateBody.js");
-const {
-  register,
-  login,
-  logout,
-  getCurrent,
-} = require("../controllers/usersController");
-const {
-  registerUserSchema,
-  loginUserSchema,
-} = require("../schemas/userSchemas.js");
+const { register, login, logout, getCurrent } = require("../controllers/usersController");
+const { registerUserSchema, loginUserSchema } = require("../schemas/userSchemas.js");
 const authMiddleware = require("../middlewares/authMiddleware.js");
+const validateBody = require("../middlewares/validateBody.js");
 
 const authRouter = express.Router();
 
--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -7,39 +7,19 @@ const {
   updateContact,
   updateStatusContact,
 } = require("../controllers/contactsControllers.js");
+const { createContactSchema, updateFavoriteSchema } = require("../schemas/contactsSchemas.js");
 
 const validateBody = require("../middlewares/validateBody.js");
 const isValidId = require("../middlewares/isValidId.js");
-
-const {
-  createContactSchema,
-  updateFavoriteSchema,
-} = require("../schemas/contactsSchemas.js");
-
 const authMiddleware = require("../middlewares/authMiddleware.js");
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", authMiddleware, getAllContacts);
-
-contactsRouter.get("/:id", isValidId, getOneContact);
-
+contactsRouter.get("/:id", authMiddleware, isValidId, getOneContact);
 contactsRouter.delete("/:id", authMiddleware, isValidId, deleteContact);
-
-contactsRouter.post(
-  "/",
-  authMiddleware,
-
-  validateBody(createContactSchema),
-  createContact
-);
-
-contactsRouter.put("/:id", validateBody(createContactSchema), updateContact);
-contactsRouter.patch(
-  "/:id/favorite",
-  isValidId,
-  validateBody(updateFavoriteSchema),
-  updateStatusContact
-);
+contactsRouter.post("/", authMiddleware, validateBody(createContactSchema), createContact);
+contactsRouter.put("/:id", authMiddleware, validateBody(createContactSchema), updateContact);
+contactsRouter.patch("/:id/favorite", authMiddleware, isValidId, validateBody(updateFavoriteSchema), updateStatusContact);
 
 module.exports = contactsRouter;
